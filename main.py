986acd2ff20069f4f98a7c0190083d993a692bba
William Suarez <147040637+Suarwill@users.noreply.github.com>
2024-10-23 15:50:03 -0300

    Actualizacion and rename Benny.py to main.py

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
-# Configuracion de librerias ----------------------------------------------------------------------
-import importlib as ilib
-import os
-import time
-import subprocess as sub
-import base64 as b6
+# ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═
+# Configuracion de librerias
+# ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═
 
-# Funcion para instalar automaticamente librerias no existentes 
+import importlib as ilib
+import subprocess as sub
 def libSetup(lib):
+    # Funcion para instalar automaticamente librerias no existentes
     try:ilib.import_module(lib)
     except ImportError:sub.check_call(['pip', 'install', lib])
 
+import os
+import time
+import base64 as b6
+
 libSetup('getpass')
 import getpass as gp
+
 libSetup('hashlib')
 import hashlib as hl
+
 libSetup('tkinter')
 from tkinter import *
 from tkinter import messagebox
+
 libSetup('pandas')
 import pandas as pd
+
 libSetup('warnings')
 import warnings
+
 libSetup('selenium')
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from multiprocessing import Pool
 
-# Desarrollo de funciones propias -----------------------------------------------------------------
+# ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═
+# Desarrollo de funciones propias
+# ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═
 def clear():
   if os.name == 'nt':  # Si es Windows
     os.system('cls')
@@ -75,9 +86,8 @@ def clsfSS():
     time.sleep(1)
     renombrar(archivos_xlsx[0], ss[3])
 
-    # Finalizar
     print("Proceso finalizado.")
-    time.sleep(5)
+    time.sleep(3)
 
 def exDif():
     clear()
@@ -94,18 +104,18 @@ def exDif():
 
     login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "btnSubmit")))
     login_button.click()
-    time.sleep(2)
     print("Acceso Conseguido")
+    time.sleep(2)
 
     sucursales = {
-#    '1003': 'AJUSTES',
+#   '1003': 'AJUSTES',
     '56': 'Alameda 2', 
     '1': 'Antofagasta 1', '21': 'Antofagasta 2', '28': 'Antofagasta 3', '63': 'Antofagasta 4', '82': 'Antofagasta 5', '111': 'Antofagasta 6', '112': 'Antofagasta 7', '137': 'Antofagasta 8',
     '79': 'Arauco Maipu 1', '97': 'Arauco Maipu 2',
     '61': 'Arica 1', '67': 'Arica 2', '107': 'Arica 3', '135': 'Arica 4',
     '93': 'Buenaventura 1',
     '10': 'Calama 1', '23': 'Calama 3', '66': 'Calama 4', '105': 'Calama 5', '118': 'Calama 6', '119': 'Calama 7',
-#    '100': 'Casa Matriz',
+#   '100': 'Casa Matriz',
     '40': 'Castro 1', '95': 'Castro 2', '128': 'Castro 3',
     '30': 'Centro Conce 2',
     '146': 'Chicureo 1',
@@ -115,23 +125,22 @@ def exDif():
     '108': 'Coquimbo 1', '110': 'Coquimbo 2', '123': 'Coquimbo 3',
     '84': 'Coronel 1', '114': 'Coronel 2',
     '140': 'Costanera Center 1',
-#    '1004': 'DEVOLUCIONES',
-    '29': 'Egaña 1', '34': 'Egaña 2', '59': 'Egaña 3', '72': 'Egaña 4', '99': 'Egaña 5',
+#   '1004': 'DEVOLUCIONES',
+    '29': 'Egaña 1', '34': 'Egaña 2', '59': 'Egaña 3', '72': 'Egaña 4', '99': 'Egaña 5', 
+#    '152': 'Egaña 6',
     '131': 'El Alba 1',
     '68': 'Independencia 1', '69': 'Independencia 2', '101': 'Independencia 3',
     '24': 'Iquique 2', '64': 'Iquique 3',
+#    '153': 'Irarrazabal 1',
     '60': 'La Calera 1',
     '7': 'La Serena 1', '18': 'La Serena 2', '22': 'La Serena 3', '48': 'La Serena 4', '102': 'La Serena 5', '122': 'La Serena 6',
+#    '155': 'Lider Oeste 1',
     '16': 'Los Angeles 1',
     '50': 'Los Dominicos 1', '109': 'Los Dominicos 2',
-    '120': 'Macul 1',
-    '129': 'Maipu 1',
-    '138': 'Maule 1',
-    '76': 'Megacenter 1',
-    '127': 'Melipilla 1',
-    '132': 'Mut 1',
+    '120': 'Macul 1', '129': 'Maipu 1', '138': 'Maule 1',
+    '76': 'Megacenter 1', '127': 'Melipilla 1', '132': 'Mut 1',
     '9': 'Norte 1', '62': 'Norte 2',
-    '8': 'Oeste 1', '58': 'Oeste 2',
+    '8': 'Oeste 1', '58': 'Oeste 2', '151': 'Oeste 3',
     '142': 'Osorno 1',
     '73': 'Ovalle 1', '126': 'Ovalle 2', '147': 'Ovalle 3',
     '78': 'Parque Arauco 1', '96': 'Parque Arauco 2', '113': 'Parque Arauco 3',
@@ -139,13 +148,11 @@ def exDif():
     '4': 'Puerto Montt 1', '25': 'Puerto Montt 2', '35': 'Puerto Montt 3', '43': 'Puerto Montt 4', '44': 'Puerto Montt 5', '45': 'Puerto Montt 6', '49': 'Puerto Montt 7', '52': 'Puerto Montt 8', '130': 'Puerto Montt 9',
     '41': 'Puerto Varas 1',
     '13': 'Punta Arenas 1','70': 'Punta Arenas 2','117': 'Punta Arenas 3',
-    '145': 'Quillota 1',
-    '57': 'Rancagua 1',
-    '116': 'Recoleta 1',
+#    '154': 'Quilin 1', 
+    '145': 'Quillota 1', '57': 'Rancagua 1', '116': 'Recoleta 1',
     '85': 'San Antonio 1','133': 'San Antonio 2','148': 'San Antonio 3',
-    '71': 'San Felipe 1',
-    '115': 'San Fernando 1',
-#    '1005': 'TRANSITO',
+    '150': 'San Bernardo 1', '71': 'San Felipe 1', '115': 'San Fernando 1',
+#   '1005': 'TRANSITO',
     '143': 'Temuco 1',
     '15': 'Tobalaba 1',
     '3': 'Trebol 1','26': 'Trebol 2','54': 'Trebol 3','75': 'Trebol 4','81': 'Trebol 5','98': 'Trebol 6','136': 'Trebol 7',
@@ -153,26 +160,64 @@ def exDif():
     '6': 'Vespucio 1','47': 'Vespucio 2',
     '2': 'Viña 1','90': 'Viña 10','88': 'Viña 11','92': 'Viña 12',
 #    '94': 'Viña 14',
-    '103': 'Viña 15','104': 'Viña 16','11': 'Viña 2','27': 'Viña 3','32': 'Viña 5','39': 'Viña 7','46': 'Viña 8','86': 'Viña 9',
+    '103': 'Viña 15','104': 'Viña 16','11': 'Viña 2','27': 'Viña 3','32': 'Viña 5', '39': 'Viña 7', '46': 'Viña 8', '86': 'Viña 9',
     '141': 'Ñuñoa 1'
     }
+
     init = '56' # Primera Opcion de sucursal
     index = list(sucursales.keys()).index(init)
     for i in range(index, len(sucursales)):
         pdv = list(sucursales.items())[i]
         reportDif(pdv[0],driver)
-
+    clear()
     return print("Obtención de reportes finalizada.")
 
 def unitZon():
-    clear()
+    # Unificado de archivos en uno SOLO
     dir = os.getcwd()
-    mix(dir)
+    df_final = pd.DataFrame()
+    filesExcel = buscXLSX(dir)
+    for x in filesExcel:
+        try:
+            df = pd.read_excel(os.path.join(dir, x), header=5, usecols='A:E')  # Leer desde la fila 6
+            df = df.dropna(how='all')  # Eliminar filas vacías
+            df['Archivo'] = x  # Agregar columna con el nombre del archivo
+            df_final = pd.concat([df_final, df], ignore_index=True)
+        except Exception as e:
+            print(f"Error al procesar el archivo {x}: {e}")
+    df_final.to_excel("unificados.xlsx", index=False)
+    clear()    
+    return print("Unificacion realizada.")
 
 def renDif():
-    clear()
+    # Renombrar todos los "Inventario" y colocar el nombre de su sucursal
     dir =  os.getcwd()
-    renomDif(dir) 
+    columna = 0
+    hoja = 'sphinx'
+    sep = '/'
+    xlsxs = buscXlsxDif(dir)
+    for x in xlsxs:
+        try:
+            # Leer el archivo Excel sin especificar el encabezado
+            df = pd.read_excel(x, sheet_name=hoja, nrows=5, header=None)
+            # Verificar si la fila 4 existe y tiene un valor en la columna especificada
+            if df.shape[0] >= 4 and not pd.isna(df.iloc[3][columna]):
+                valor_celda = df.iloc[3][columna]
+                # Dividir el valor por el separador y tomar la segunda parte
+                nueva_parte = valor_celda.split(sep)[1].strip()
+                # Construir el nuevo nombre de archivo
+                nombre_directorio, extension = os.path.splitext(x)
+                nuevo_nombre = nueva_parte + extension
+                # Renombrar el archivo
+                os.rename(x, nuevo_nombre)
+                print(f"Archivo renombrado a: {nuevo_nombre}")
+            else:
+                print(f"La fila 4 en la hoja '{hoja}' del archivo {x} está vacía o no existe.")
+        except (FileNotFoundError, PermissionError, IndexError, ValueError) as e:
+            print(f"Error al procesar el archivo {x}: {e}")
+        time.sleep(1)
+    clear()
+    return print("Renombrado finalizado.") 
 
 def buscXLSX(dir):
     list = os.listdir(dir)
@@ -205,77 +250,40 @@ def renombrar(dir, newName):
     except OSError as error:
         print(f"Error al renombrar el archivo: {error}")
 
-def renomDif(dir):
-    columna = 0
-    hoja = 'sphinx'
-    sep = '/'
-    xlsxs = buscXlsxDif(dir)
-    for x in xlsxs:
-        try:
-            # Leer el archivo Excel sin especificar el encabezado
-            df = pd.read_excel(x, sheet_name=hoja, nrows=5, header=None)
-            # Verificar si la fila 4 existe y tiene un valor en la columna especificada
-            if df.shape[0] >= 4 and not pd.isna(df.iloc[3][columna]):
-                valor_celda = df.iloc[3][columna]
-                # Dividir el valor por el separador y tomar la segunda parte
-                nueva_parte = valor_celda.split(sep)[1].strip()
-                # Construir el nuevo nombre de archivo
-                nombre_directorio, extension = os.path.splitext(x)
-                nuevo_nombre = nueva_parte + extension
-                # Renombrar el archivo
-                os.rename(x, nuevo_nombre)
-                print(f"Archivo renombrado a: {nuevo_nombre}")
-            else:
-                print(f"La fila 4 en la hoja '{hoja}' del archivo {x} está vacía o no existe.")
-        except (FileNotFoundError, PermissionError, IndexError, ValueError) as e:
-            print(f"Error al procesar el archivo {x}: {e}")
+def reportDif (sucursal,driver):
+    try:
+        select_element  = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "Sphinx_Sucursales")))
+        select = Select(select_element)
+        select.select_by_value(str(sucursal))
         time.sleep(1)
 
-def mix(dir):
-    df_final = pd.DataFrame()
-    for archivo in os.listdir(dir):
-        if archivo.endswith('.xlsx') or archivo.endswith('.xls'):
-            try:
-                df = pd.read_excel(os.path.join(dir, archivo), header=5, usecols='A:E')  # Leer desde la fila 6
-                df = df.dropna(how='all')  # Eliminar filas vacías
-                df['Archivo'] = archivo  # Agregar columna con el nombre del archivo
-                df_final = pd.concat([df_final, df], ignore_index=True)
-            except Exception as e:
-                print(f"Error al procesar el archivo {archivo}: {e}")
-    df_final.to_excel("unificados.xlsx", index=False)
+        select_element  = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "inventario_tipo")))
+        select = Select(select_element)
+        select.select_by_value("2") # Productos con Diferencias
+        time.sleep(1)
 
-def reportDif (sucursal,driver):
-    select_element  = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "Sphinx_Sucursales")))
-    select = Select(select_element)
-    select.select_by_value(str(sucursal))
-    time.sleep(1)
+        login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "btnEjecuta")))
+        login_button.click()
+        time.sleep(2)
 
-    select_element  = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "inventario_tipo")))
-    select = Select(select_element)
-    select.select_by_value("2") # Productos con Diferencias
-    time.sleep(1)
-
-    login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "btnEjecuta")))
-    login_button.click()
-    time.sleep(2)
-
-    login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "btnExcel")))
-    login_button.click()
-    time.sleep(1)
-    print(sucursal)
-    return
+        login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "btnExcel")))
+        login_button.click()
+        time.sleep(1)
+        print(sucursal)
+    except (TimeoutException, NoSuchElementException) as e:
+        print(f"Error al descargar informe {sucursal}: {e}")
+        
+    return None
 
 def processM(Xlsxs, hoja , newFile, dir):
     directorio = dir
-    # Procesar y renombrar cada archivo XLSX
     for i in Xlsxs:
         procesarMermas(i, hoja, newFile, directorio)
 
 def processS(Xlsxs, hoja ,saltar, newFile, dir):
     directorio = dir
-    # Procesar y renombrar cada archivo XLSX
     for i in Xlsxs:
-        procesarSS(i, hoja, saltar, newFile)
+        procesarSS(i, hoja, saltar, newFile, directorio)
 
 def procesarMermas(ruta, hoja, newFile,directorio):
     try:
@@ -332,7 +340,7 @@ def exec(file):
     except FileNotFoundError:
         messagebox.showerror("Error", "No se encontró el archivo raíz")
 
-def doit(ws):
+def windows(ws):
     tkp = gp.getpass(decB64("SW5zZXJ0IEFQSS1LZXk6IA=="))
     try:
         dataBase = chskp(tkp)
@@ -357,13 +365,17 @@ def chskp(tkp):
     else:
         return False
 
+# ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═
 # Configuración de la ventana principal
+# ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═
 ws = Tk()
 ws.title(decB64("VmVudGFuYSBkZSBUcmFiYWpv"))
 
+warnings.filterwarnings("ignore", category=UserWarning)
+
 # Configuración de tamaño y posición de la ventana
 width, heigth = 800, 300
-pMedW , pMedH , auth= int((ws.winfo_screenwidth()-width)/2), int((ws.winfo_screenheight()-heigth)/2), doit(ws)
+pMedW , pMedH , pAuth = int((ws.winfo_screenwidth()-width)/2), int((ws.winfo_screenheight()-heigth)/2), windows(ws)
 ws.geometry(f"{width}x{heigth}+{pMedW}+{pMedH}")
 
 # Variables de posicionamiento
@@ -394,7 +406,10 @@ ws.grid_columnconfigure(2, weight=1)
 ws.grid_columnconfigure(3, weight=1)
 ws.grid_columnconfigure(4, weight=1)
 
-# Variables funcionales
-
 # Bucle
 ws.mainloop()
+
+# ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═
+# Crear Paquete EXE
+# pyinstaller --onefile main.py
+# ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═
